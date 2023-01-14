b0a709bd2d741d3bb8694e302da3862e9f62d342
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2023-01-13 22:31:51 -0300

    Subindo arquivos iniciais

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Menu from './componentes/Menu';
+import Inicio from './paginas/Inicio';
+import SobreMim from './paginas/SobreMim';
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path='/' element={<Inicio />} />
+        <Route path='/sobremim' element={<SobreMim/>} />
+        <Route path='*' element={<div>Pagina nao encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
